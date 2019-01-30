4fab939e22449cfc3cc67d7d42be9165d7261668
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 20:37:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 require "pry"
 def countdown(int)
-  x = int
   i = 0 
   while i < int 
   puts "#{int} SECOND(S)!"
-  x -= 1 
+  int -= 1 
   i += 1 
 end 
   "HAPPY NEW YEAR!"
