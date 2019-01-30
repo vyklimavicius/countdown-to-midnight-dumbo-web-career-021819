6e9e9d9360ba26589466cd838f85310e7db5f8d9
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 20:45:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,6 @@
 #write your code here
 require "pry"
 def countdown(int)
-  
   while int > 0 
   puts "#{int} SECOND(S)!"
   int -= 1 
@@ -9,13 +8,13 @@ end
   "HAPPY NEW YEAR!"
 end
 
-# def countdown_with_sleep(int)
-#   i = 0 
-#   while i < int 
-#   puts "#{int} SECOND(S)!"
-#   int -= 1 
-# end 
-#   "HAPPY NEW YEAR!"
-# end 
+def countdown_with_sleep(int)
+  i = 0 
+  while i < int 
+  puts "#{int} SECOND(S)!"
+  int -= 1 
+end 
+  "HAPPY NEW YEAR!"
+end 
 
 
