288d1676997e4163f5bb94c8dddf5711141bfead
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 20:44:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,13 @@ end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(int)
-   i = 0 
-  while i < int 
-  puts "#{int} SECOND(S)!"
-  int -= 1 
-end 
-  "HAPPY NEW YEAR!"
-end 
+# def countdown_with_sleep(int)
+#   i = 0 
+#   while i < int 
+#   puts "#{int} SECOND(S)!"
+#   int -= 1 
+# end 
+#   "HAPPY NEW YEAR!"
+# end 
 
 
