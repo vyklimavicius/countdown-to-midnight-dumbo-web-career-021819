d0617989075464bf665c870e1b9a311cec8b46d3
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 20:47:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+require "pry"
+def countdown(int)
+  while int > 0 
+  puts "#{int} SECOND(S)!"
+  int -= 1 
+end 
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  while int > 0 
+  puts "#{int} SECOND(S)!"
+  sleep(int)
+  int -= 1 
+end 
+  "HAPPY NEW YEAR!"
+end 
+
+
